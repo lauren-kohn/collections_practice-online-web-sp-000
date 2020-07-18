7e719d5ed67c0209255ef97cbf4fc2b743267b12
Lauren Kohn <lauren.kohn@soft-adv.com>
2020-07-18 22:12:33 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -38,7 +38,7 @@ def sum_array(array)
 end
 
 def add_s(array)
-  array.each_with_index.collect do |word, i|
+  array.each_with_index do |word, i|
     word[word.length] = "s" unless i == 1 
   end
 end 
