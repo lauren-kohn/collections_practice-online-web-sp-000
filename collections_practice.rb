e1d83fa56240d1cc79b073a7377d531b980e620c
Lauren Kohn <lauren.kohn@soft-adv.com>
2020-07-18 21:37:10 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -17,7 +17,8 @@ end
 def swap_elements_from_to(array, index, destination_index)
   temp = array(index)
   array(index) = array(destination_index)
-  array(destination_index) = 
+  array(destination_index) = temp
+  array
 end 
 
 def reverse_array(array)
